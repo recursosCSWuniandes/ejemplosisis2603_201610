807c65b05117764e2d81efbd0d149e6a458d9724
Diego F. Ruiz S <yego23@gmail.com>
2016-03-02 10:04:36 -0500

    Borrado frontend

--- FrontEnd-Cities/src/main/webapp/src/modules/city/city.mod.js
+++
@@ -1,9 +0,0 @@
-
-(function(ng){
-    
-    var mod = ng.module("cityModule", ["ui.bootstrap"]);
-    
-    mod.constant("cityContext", "http://localhost:8080/Servidor-JAXRS/api/cities/");
-    
-})(window.angular);
-
